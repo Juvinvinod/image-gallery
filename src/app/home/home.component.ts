ea1a521e487e9d02160344a53e68cce0d742aa1a
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2024-05-11 05:46:58 +0530

    Added comments and error handler in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Component,
   NgZone,
+  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
@@ -11,6 +12,7 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import {
   BehaviorSubject,
+  Subscription,
   filter,
   map,
   pairwise,
@@ -26,12 +28,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit, AfterViewInit {
-  @ViewChild(CdkVirtualScrollViewport) scroller!: CdkVirtualScrollViewport;
-  loading = false;
-  pageNumber = 1;
-  displayType!: string;
-  isLargeScreen!: boolean;
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(CdkVirtualScrollViewport) scroller!: CdkVirtualScrollViewport; //for infinite scroll
+  loading = false; // hide the loader by default
+  pageNumber = 1; // page number used for fetching data
+  displayType!: string; // grid or list
+  isLargeScreen!: boolean; // to make grid responsive
+  imageSubscription!: Subscription;
 
   // Declare imageList$ as a BehaviorSubject
   imageList$: BehaviorSubject<Photos[]> = new BehaviorSubject<Photos[]>([]);
@@ -44,20 +47,25 @@ export class HomeComponent implements OnInit, AfterViewInit {
     private snackBar: MatSnackBar
   ) {}
 
+  //show only 1 col per row in grid view on smaller screens and 3 in larger screen.
   getGridCols(): number {
     return this.breakpointObserver.isMatched('(max-width: 1024px)') ? 1 : 3;
   }
 
+  //save displayType to variable when the toggle button is clicked.
   onViewTypeChange(viewType: string) {
     this.displayType = viewType;
+    this.ngAfterViewInit();
   }
 
+  //fetch next pages containing data
   fetchMore() {
     this.loading = true;
     this.pageNumber++;
     this.getImages(this.pageNumber);
   }
 
+  // check if data already exists in local storage else make the api call.
   getImages(pageNumber: number): void {
     const localItem = this.galleryService.getFromLocalStorage(pageNumber);
     if (localItem?.length !== 0) {
@@ -65,20 +73,29 @@ export class HomeComponent implements OnInit, AfterViewInit {
       console.log(this.imageList$.value);
       this.loading = true;
     } else {
-      this.galleryService.getDataFromApi(pageNumber).subscribe({
-        next: res => {
-          this.imageList$.next([...this.imageList$.value, ...res]);
-          this.galleryService.saveToLocalStorage(res);
-          this.loading = true;
-        },
-      });
+      this.imageSubscription = this.galleryService
+        .getDataFromApi(pageNumber)
+        .subscribe({
+          next: res => {
+            this.imageList$.next([...this.imageList$.value, ...res]);
+            this.galleryService.saveToLocalStorage(res);
+            this.loading = true;
+          },
+          error: () => {
+            this.snackBar.open('Error fetching data!!', 'Dismiss', {
+              duration: 3000,
+            });
+          },
+        });
     }
   }
 
+  //load images initially when the component is loaded
   ngOnInit(): void {
     this.getImages(this.pageNumber);
   }
 
+  //check if the user is scrolling down,once reached bottom fetch more data
   ngAfterViewInit(): void {
     timer(5000).subscribe(() => {
       this.scroller
@@ -100,6 +117,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     });
   }
 
+  //remove the swiped image details from local storage using the image id.
   drop(event: CdkDragDrop<Photos[]>) {
     const currentIndex = event.currentIndex;
     const currentValue = this.imageList$.value;
@@ -114,21 +132,31 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
       // Update the BehaviorSubject with the filtered list
       this.imageList$.next(updatedList);
-      console.log(updatedList);
       this.snackBar.open('Item deleted!!', 'OK', {
         duration: 3000,
       });
     } else {
       console.error('Invalid currentIndex:', currentIndex);
+      this.snackBar.open('Unknown Error!!', 'Dismiss', {
+        duration: 3000,
+      });
     }
   }
 
+  //trackBy function for better performance when dealing with long lists.
   trackByFn(index: number, item: Photos): string {
     return item.id;
   }
 
+  //navigate user to next page when clicked on the list.
   onListItemClick(item: Photos) {
-    // Assuming item.id contains the ID needed for navigation
     this._router.navigate(['/details', item.id]);
+  }
+
+  //unsubscribe when changing component
+  ngOnDestroy(): void {
+    if (this.imageSubscription) {
+      this.imageSubscription.unsubscribe();
+    }
   }
 }
